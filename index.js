761a33812e0572f9a0babb9cccdef1f113983b5a
Achraf <achrafhafid565@gmail.com>
2025-03-25 03:39:51 +0100

    changing the port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 const cors = require("cors");
 const sharp = require("sharp");
 app.use(cors());
