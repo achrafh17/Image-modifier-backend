a3ad78d9b3540411785fa7af42fd52f6c28369c8
Achraf <achrafhafid565@gmail.com>
2025-03-25 02:55:37 +0100

    fix

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3000;
 const cors = require("cors");
 const sharp = require("sharp");
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
 
 //------------------------------------------------------------
 app.post("/app", async (req, res) => {
